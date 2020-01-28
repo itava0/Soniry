b592db7b0f85c065cae08a9d324bfd0ab9dc0de3
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2020-01-28 15:05:53 -0500

    Create the navbar component for the page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,28 @@
-import React, { Component } from 'react'; 
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 class Navbar extends Component {
-  state = {  }
-  render() { 
+  state = {};
+  render() {
     return (
-      <div>
-        <h3>Hello from Navbar</h3>
-      </div>
+      <nav className="nav">
+        <Link className="nav__logo-name" to="/">soniry</Link>
+            <Link to="/">
+              products
+            </Link>
+            <form className="search">
+        <input
+          className="search__input"
+          type="text"
+          placeholder="Search Pruduct..."
+        />
+      </form>
+        <Link to="/cart">
+           <i className="fas fa-cart-plus"></i> <span>1</span>
+        </Link>
+      </nav>
+
     );
   }
 }
- 
+
 export default Navbar;
